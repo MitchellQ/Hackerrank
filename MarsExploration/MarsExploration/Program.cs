89a027cac212df930b37bb06425278049c8b28d6
MitchellQ <mitchell.quarrie@outlook.com>
2020-05-04 18:50:03 +1200

    Start of solution
    Doesn't resolve edge cases

---
+++ MarsExploration/MarsExploration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MarsExploration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int result = MarsExploration(s);
+
+            Console.WriteLine(result);
+        }
+
+        private static int MarsExploration(string s)
+        {
+        }
+
+        //private static int MarsExploration(string s) => s.Length - ((s.Length - s.Replace("S", "").Length) + (s.Length - s.Replace("O", "").Length));
+
+
+    }
+}
