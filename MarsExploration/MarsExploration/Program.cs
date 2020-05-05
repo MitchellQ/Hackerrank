d2badb7d4934f88995949a77af596b6fc443729d
MitchellQ <mitchell.quarrie@outlook.com>
2020-05-05 19:16:26 +1200

    Solved problem

--- MarsExploration/MarsExploration/Program.cs
+++ MarsExploration/MarsExploration/Program.cs
@@ -18,10 +18,21 @@ namespace MarsExploration
             Console.WriteLine(result);
         }
 
+
         private static int MarsExploration(string s)
         {
-        }
+            int count = 0;
+            for(int i = 0; i < s.Length; i += 3)
+            {
+                if (s[i] != 'S') count++;
+                if (s[i + 1] != 'O') count++;
+                if (s[i + 2] != 'S') count++;
+            }
 
+            return count;
+        }
+        
+        //Didn't work on some edge cases 
         //private static int MarsExploration(string s) => s.Length - ((s.Length - s.Replace("S", "").Length) + (s.Length - s.Replace("O", "").Length));
 
 
